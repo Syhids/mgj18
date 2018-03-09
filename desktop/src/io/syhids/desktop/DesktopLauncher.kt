1904492092662bd9751b51f5d9d8e1fb76ed6a44
Manuel Jiménez <mjimenez@habitissimo.com>
2018-03-09 22:32:03 +0100

    Adds ktx libraries Adds bg

---
+++ desktop/src/io/syhids/desktop/DesktopLauncher.kt
@@ -0,0 +1,16 @@
+package io.syhids.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import io.syhids.MyGdxGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        config.width = 800
+        config.height = 600
+        config.fullscreen = false
+        LwjglApplication(MyGdxGame(), config)
+    }
+}
