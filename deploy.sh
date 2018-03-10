bf6731349a911ba452ebeb35d68505c9f4cbe4f3
Manuel Jiménez <mjimenez@habitissimo.com>
2018-03-10 13:36:04 +0100

    Deploy script works better

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 ./gradlew desktop:dist
+mkdir -p ./bin
 mv desktop/build/libs/desktop-1.0.jar ./bin/game.jar
 cp play.sh ./bin/
 cp play.bat ./bin/
+
+mkdir -p ./bin/assets/
+cp assets/* ./bin/assets/
 cd bin
-rm game.zip
+rm -f game.zip
 zip -r game.zip .
-rm game.jar
-rm play.bat
-rm play.sh
-nautilus . &
+#rm -f game.jar
+#nautilus . &
 cd ..
 echo "Done"
