666e5fbe4fb04140fa940b0eba252e2defbe226c
Lluc Sansaloni <ll.sansaloni@gmail.com>
2018-03-10 11:53:52 +0100

    changed the respawn time of enemies

--- core/src/io/syhids/mgj18/system/SpawnEnemySystem.kt
+++ core/src/io/syhids/mgj18/system/SpawnEnemySystem.kt
@@ -7,6 +7,7 @@ import io.syhids.mgj18.CanSpawnComponent
 import io.syhids.mgj18.Enemy
 import io.syhids.mgj18.component
 import io.syhids.mgj18.position
+import java.util.*
 
 class SpawnEnemySystem : IteratingSystem(Family.all(
         CanSpawnComponent::class.java
@@ -14,18 +15,21 @@ class SpawnEnemySystem : IteratingSystem(Family.all(
     // private val position = component(PositionComponent::class)
     private val canSpawn = component(CanSpawnComponent::class)
     var accTime = 0f
+    val rnd = Random()
 
     override fun processEntity(entity: Entity, deltaTime: Float) {
 
         val canSpawnComponent = canSpawn.get(entity)
         canSpawnComponent.accTime += deltaTime
-        val minTime = 2
+        val minTime = 0
+
+
         if (canSpawnComponent.accTime >= minTime) {
             val enemy = Enemy()
             enemy.position.x = entity.position.x
             enemy.position.y = entity.position.y
             engine.addEntity(enemy)
-            canSpawnComponent.accTime -= minTime
+            canSpawnComponent.accTime -= 10 + rnd.nextFloat() * 10
         }
 
 
