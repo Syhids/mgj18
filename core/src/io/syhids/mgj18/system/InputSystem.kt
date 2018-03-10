190cd89f593195fc5143fd7c9da8b58657b292f3
Manuel Jiménez <mjimenez@habitissimo.com>
2018-03-10 09:01:40 +0100

    Uses velocity instead of position for hero movement

--- core/src/io/syhids/mgj18/system/InputSystem.kt
+++ core/src/io/syhids/mgj18/system/InputSystem.kt
@@ -5,29 +5,29 @@ import com.badlogic.ashley.core.Family
 import com.badlogic.ashley.systems.IteratingSystem
 import com.badlogic.gdx.Input.Keys
 import io.syhids.mgj18.KeyboardAffectedComponent
-import io.syhids.mgj18.PositionComponent
+import io.syhids.mgj18.VelocityComponent
 import io.syhids.mgj18.anyKeyPressed
 import io.syhids.mgj18.component
 
 class InputSystem : IteratingSystem(Family.all(
-    PositionComponent::class.java,
+    VelocityComponent::class.java,
     KeyboardAffectedComponent::class.java
 ).get()) {
-    private val position = component(PositionComponent::class)
+    private val velocity = component(VelocityComponent::class)
 
     val POWER = 50f
 
     override fun processEntity(entity: Entity, deltaTime: Float) {
-        val position = position.get(entity)
+        val velocity = velocity.get(entity)
 
         when {
-            anyKeyPressed(Keys.A, Keys.LEFT) -> position.x -= deltaTime * POWER
-            anyKeyPressed(Keys.D, Keys.RIGHT) -> position.x += deltaTime * POWER
+            anyKeyPressed(Keys.A, Keys.LEFT) -> velocity.x -= deltaTime * POWER
+            anyKeyPressed(Keys.D, Keys.RIGHT) -> velocity.x += deltaTime * POWER
         }
 
         when {
-            anyKeyPressed(Keys.W, Keys.UP) -> position.y += deltaTime * POWER
-            anyKeyPressed(Keys.S, Keys.DOWN) -> position.y -= deltaTime * POWER
+            anyKeyPressed(Keys.W, Keys.UP) -> velocity.y += deltaTime * POWER
+            anyKeyPressed(Keys.S, Keys.DOWN) -> velocity.y -= deltaTime * POWER
         }
     }
 }
